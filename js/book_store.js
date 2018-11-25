41ab43b2e3195bfa0609931aabece1933a825d88
Muhammad Usama Siddiqui <usiddiqui14@gmail.com>
2018-11-25 20:09:57 +0500

    3rd Version

--- js/book_store.js
+++ js/book_store.js
@@ -1,10 +1,6 @@
 var bookStoreDetail =[];
 var checkout = [];
-// [{bookName:"Namal",authName:"Namra Ahmad",bookSerialNo:"3310113843401",bookPrice:"1000/-"},
-//                        {bookName:"Bang e Dara",authName:"Allama Iqbal",bookSerialNo:"331019843341",bookPrice:"2000/-"},
-//                        {bookName:"Peer e Kamil",authName:"Umera Ahmad",bookSerialNo:"331019823401",bookPrice:"950/-"},
-//                        {bookName:"Abdullah",authName:"Hashim Nadeem",bookSerialNo:"331011243401",bookPrice:"1500/-"},
-//                        {bookName:"Khak o Khun",authName:"Naseem Hijazi",bookSerialNo:"3310239843401",bookPrice:"800/-"}];
+
 function add_books() {
 	var bsobj = {};
 	bookName = document.getElementById('namebook').value;
@@ -24,13 +20,14 @@ function add_books() {
 function search_books() {
 	      var input = document.getElementById('searchTerm').value;
 	      var result = "<table border=1 table table-hover table-dark>";
+
 	      for (var i = 0; i < bookStoreDetail.length; i++) {
 	      	result += "<tr>";
-	      	if ((input).toUpperCase() == (bookStoreDetail[i]).toUpperCase()) {
-	      	result += "<td>"+bookStoreDetail[i].bookName+"</td>"+"<td>"+bookStoreDetail[i].authName+"</td>"+"<td>"+bookStoreDetail[i].bookSerialNo+"</td>"+"<td>"+bookStoreDetail[i].bookPrice+"</td>";	
+	      	if (input.toLowerCase() === ((bookStoreDetail[i]).bookName).toLowerCase()||input.toLowerCase() === ((bookStoreDetail[i]).bookSerialNo).toLowerCase()) {
+	      	result += "<td>"+bookStoreDetail[i].bookName+"</td>"+"<td>"+bookStoreDetail[i].authName+"</td>"+"<td>"+bookStoreDetail[i].bookSerialNo+"</td>"+"<td>"+bookStoreDetail[i].bookPrice+"</td>"+"<td>"+bookStoreDetail[i].bookQuantity+"</td>";	
 	      	}
 	      	else{
-
+	      		console.log("Book Not Found")
 	      	}
 	      	result +="<tr>";
 	      }
@@ -63,7 +60,7 @@ function buy_book() {
 	var bkquantity = document.getElementById('bbook_quantity').value;
 	for (var i = 0; i < bookStoreDetail.length; i++) 
 	{
-		if ((bknmfld).toUpperCase() === (bookStoreDetail[i].toUpperCase())) 
+		//if ((bknmfld).toUpperCase() === (bookStoreDetail[i].toUpperCase())) 
 		{
 			
 		}
